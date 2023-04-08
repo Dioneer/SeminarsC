20ec3ef3ac80aace18daedb8536b999d549dbed0
Dioneer <Gulkina_em@mail.ru>
2023-04-08 21:26:42 +0300

    third task for homework1

--- homeworkForSeminar1/Program.cs
+++ homeworkForSeminar1/Program.cs
@@ -17,10 +17,10 @@ namespace homeworkForSeminar1
 			// Console.Write("Please, enter second number (you can use integer or decimal with comma separate): ");
 			// string text2 = Console.ReadLine();
 
-			// if (int.TryParse(text1, out int number1))
+			// if (double.TryParse(text1, out double number1) && double.TryParse(text2, out double number2))
 			// {
-			// 	number1 = Convert.ToInt32(text1);
-			// 	int number2 = Convert.ToInt32(text2);
+			// 	number1 = Convert.ToDouble(text1);
+			// 	number2 = Convert.ToDouble(text2);
 
 			// 	if (number1 > number2)
 			// 	{
@@ -33,22 +33,77 @@ namespace homeworkForSeminar1
 			// }
 			// else
 			// {
-			// 	double number3 = Convert.ToDouble(text1);
-			// 	double number4 = Convert.ToDouble(text2);
+			// 	Console.WriteLine("Please, both numbers mist be digits and have no dots");
+			// }
 
-			// 	if (number3 > number4)
+			// Console.WriteLine("If you whant to continue press any key");
+			// Console.ReadKey();
+			//===============Exesise 2=========================================================
+			//===============Exesise 4=========================================================
+			// Console.Clear();
+			// Console.WriteLine("Hello! Let's find out which of three numbers is bigger.");
+			// Console.Write("Please, enter first number (you can use integer or decimal with comma separate): ");
+			// string text1 = Console.ReadLine();
+			// Console.Write("Please, enter second number (you can use integer or decimal with comma separate): ");
+			// string text2 = Console.ReadLine();
+			// Console.Write("Please, enter third number (you can use integer or decimal with comma separate): ");
+			// string text3 = Console.ReadLine();
+
+			// if (double.TryParse(text1, out double number1) && double.TryParse(text2, out double number2) && double.TryParse(text3, out double number3))
+			// {
+			// 	number1 = Convert.ToDouble(text1);
+			// 	number2 = Convert.ToDouble(text2);
+			// 	number3 = Convert.ToDouble(text3);
+			// 	double[] array = { number1, number2, number3 };
+			// 	byte i = 1;
+			// 	double max = array[0];
+
+			// 	while (i <= 2)
 			// 	{
-			// 		Console.WriteLine($"{number3} is bigger");
+			// 		if (max < array[i])
+			// 		{
+			// 			max = array[i];
+			// 		}
+			// 		i++;
+			// 	}
+			// 	Console.WriteLine($"{max} is bigger");
+			// }
+			// else
+			// {
+			// 	Console.WriteLine("Please, all numbers must be digits and have no dots");
+			// }
+
+			// Console.WriteLine("If you whant to continue press any key");
+			// Console.ReadKey();
+			//===============Exesise 4=========================================================
+			//===============Exesise 6=========================================================
+			// Console.Clear();
+			// Console.WriteLine("Hello! Let's find out iа your number is odd or even.");
+			// Console.Write("Please, enter your number (you can use integer or decimal with comma separate): ");
+			// string enterString = Console.ReadLine();
+
+			// if (float.TryParse(enterString, out float enterNumber))
+			// {
+			// 	if (enterNumber % 2 == 0)
+			// 	{
+			// 		Console.WriteLine("Your number is even");
 			// 	}
 			// 	else
 			// 	{
-			// 		Console.WriteLine($"{number4} is bigger");
+			// 		Console.WriteLine("Your number is odd");
 			// 	}
 			// }
-
-			// Console.WriteLine("If you whant to contine press any key");
+			// else
+			// {
+			// 	Console.WriteLine("Please, your number must be digit and have no dots");
+			// }
+			// Console.WriteLine("If you whant to continue press any key");
 			// Console.ReadKey();
-			//===============Exesise 2=========================================================
+
+			//===============Exesise 6=========================================================
+			//===============Exesise 8=========================================================
+
+			//===============Exesise 8=========================================================
 		}
 	}
 }
