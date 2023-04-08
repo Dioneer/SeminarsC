45ac507e0a96c4f020e219155db93eeb634e44c4
Dioneer <Gulkina_em@mail.ru>
2023-04-08 14:11:12 +0300

    first task

---
+++ homeworkForSeminar1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+// данная структура взята по совету из дополнтельных материалов. Предполагаю, что System не требует явного объявления, но я еще до этого не дочитала, поэтому пока так =)
+
+
+namespace homeworkForSeminar1
+{
+	class homework1
+	{
+		static void Main()
+		{
+			//===============Exesise 2=========================================================
+			// Console.Clear();
+			// Console.WriteLine("Hello! Let's find out which of two numbers is bigger.");
+			// Console.Write("Please, enter first number (you can use integer or decimal with comma separate): ");
+			// string text1 = Console.ReadLine();
+			// Console.Write("Please, enter second number (you can use integer or decimal with comma separate): ");
+			// string text2 = Console.ReadLine();
+
+			// if (int.TryParse(text1, out int number1))
+			// {
+			// 	number1 = Convert.ToInt32(text1);
+			// 	int number2 = Convert.ToInt32(text2);
+
+			// 	if (number1 > number2)
+			// 	{
+			// 		Console.WriteLine($"{number1} is bigger");
+			// 	}
+			// 	else
+			// 	{
+			// 		Console.WriteLine($"{number2} is bigger");
+			// 	}
+			// }
+			// else
+			// {
+			// 	double number3 = Convert.ToDouble(text1);
+			// 	double number4 = Convert.ToDouble(text2);
+
+			// 	if (number3 > number4)
+			// 	{
+			// 		Console.WriteLine($"{number3} is bigger");
+			// 	}
+			// 	else
+			// 	{
+			// 		Console.WriteLine($"{number4} is bigger");
+			// 	}
+			// }
+
+			// Console.WriteLine("If you whant to contine press any key");
+			// Console.ReadKey();
+			//===============Exesise 2=========================================================
+		}
+	}
+}
