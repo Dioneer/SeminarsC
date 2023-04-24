1c5ac781d9ef85726189bcc7e2da8d5449fd6963
Dioneer <Gulkina_em@mail.ru>
2023-04-24 13:29:05 +0300

    homework5 exe34

---
+++ homeworkForSeminar5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Homework5
+{
+	class Programm5
+	{
+		static void Main()
+		{
+			// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+			//======= Сами функции в файле exe34.cs==================
+			// System.Console.WriteLine("let's find out the number of even numbers in the array.");
+			// System.Console.WriteLine("Please enter length of the array: ");
+			// int length = Convert.ToInt32(Console.ReadLine());
+			// PlusCounter counter = new PlusCounter();
+			// int[] randomArr = counter.randomArray(length);
+			// counter.showArr(randomArr);
+			// int result = counter.counter(randomArr);
+			// System.Console.WriteLine($"The number of even numbers is: {result}");
+			//======= Сами функции в файле exe34.cs==================
+		}
+	}
+}
