5df883d0ba5e608af22c2ec54d9309bbbf96bcdf
Dioneer <Gulkina_em@mail.ru>
2023-06-10 23:49:17 +0300

    quick sort

--- Seminar9/Program.cs
+++ Seminar9/Program.cs
@@ -30,6 +30,18 @@ namespace Seminar7
 			// int num2 = Convert.ToInt32(Console.ReadLine());
 			// double result = SummNumbers(num1, num2);
 			// System.Console.WriteLine(result);
+
+			//=========================================================================
+			Console.Clear();
+			Console.Write("Введите кол-во элементов: ");
+			int n = int.Parse(Console.ReadLine()!);
+			int[] array = new int[n];
+			for (int i = 0; i < array.Length; i++)
+				array[i] = new Random().Next(1, 101);
+			Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+			Quicksort(array, 0, array.Length - 1);
+			Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
+			//=========================================================================
 		}
 		// public static void ShowNumbers(int number)
 		// {
@@ -54,5 +66,34 @@ namespace Seminar7
 		// 	if (num2 < 0 && num2 != 0) return SummNumbers(num1, num2 + 1) / num1;
 		// 	return 1;
 		// }
+		//=========================================================================
+		public static int Partition(int[] array, int start, int end) // [1, 2, 4, 5, 3] 2 4
+		{
+			int marker = start; // Индекс опорного элемента 2
+			for (int i = start; i < end; i++)
+			{
+				if (array[i] < array[end])
+				{
+					(array[marker], array[i]) = (array[i], array[marker]);
+					marker++; // Получаем индекс числа большего, чем последнее + меняем местами числа, меньшие последнего, выносим в начало.
+				}
+			}
+			// Мы получили индекс большего и поменяли местами больший с последним, последний больше предыдущих, тк был if
+			(array[marker], array[end]) = (array[end], array[marker]);
+			return marker;
+		}
+
+
+		public static void Quicksort(int[] array, int start, int end) // [5, 3, 4, 1, 2] 0 4
+		{
+			if (start >= end)
+				return;
+
+			int pivot = Partition(array, start, end); //индекс клочества замен в if
+			System.Console.WriteLine(pivot);
+			Quicksort(array, start, pivot - 1);
+			Quicksort(array, pivot + 1, end); // [1, 2, 3, 4, 5] 3 4
+		}
+		//=========================================================================
 	}
 }
